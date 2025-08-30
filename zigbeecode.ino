fe71ddc15e381a4b8d6a0e3a964cf15201966c30
sssong-netizen <principle2607@gmail.com>
2025-08-30 15:40:35 +0900

    Update zigbeecode.ino

--- zigbeecode.ino
+++ zigbeecode.ino
@@ -1,15 +1,9 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial Zigbee(0, 1); // RX, TX
-
 void setup() {
   Serial.begin(9600);
-  Zigbee.begin(9600);
-  Serial.println("Zigbee Ready");
 }
 
 void loop() {
-  Zigbee.println("Hello from Arduino!");
-  Serial.println("ok");  //ok 신호보냄
-  delay(2000);
+  Serial.println("OK");
+  delay(1000);
 }
+
